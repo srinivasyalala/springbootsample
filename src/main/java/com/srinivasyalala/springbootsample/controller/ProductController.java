deb598a13b118f6e56eef5efaee35597198e125c
srini <srinivas.yalala@gmail.com>
2022-10-28 14:10:45 -0500

    Sample

--- src/main/java/com/srinivasyalala/springbootsample/controller/ProductController.java
+++ src/main/java/com/srinivasyalala/springbootsample/controller/ProductController.java
@@ -6,7 +6,12 @@ import com.srinivasyalala.springbootsample.model.Product;
 import com.srinivasyalala.springbootsample.service.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static java.lang.Boolean.TRUE;
+
 @RestController
 public class ProductController
 {
@@ -16,9 +21,18 @@ public class ProductController
     @GetMapping(value = "/product")
     public List<Product> getProduct()
     {
-//finds all the products
+        //finds all the products
         List<Product> products = productService.findAll();
-//returns the product list
+        //returns the product list
         return products;
+    }
+    @GetMapping(value = "/productbyid/{id}")
+    public List<Product> getProductbyID(@PathVariable long id)
+    {
+        //finds all the products
+        List<Product> products = productService.findByID(id);
+        //returns the product list
+        return products;
+        //return TRUE;
     }
 }
--- src/main/java/com/srinivasyalala/springbootsample/service/IProductService.java
+++ src/main/java/com/srinivasyalala/springbootsample/service/IProductService.java
@@ -6,4 +6,5 @@ import java.util.List;
 public interface IProductService
 {
     List<Product> findAll();
+    List<Product> findByID(long id);
 }
